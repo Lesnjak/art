a295e4973ed58b87ce7740cfed5f3e755b957f8f
Lesnjak <vnachalesobaka@gmail.com>
2018-06-03 23:54:50 +0300

    newc

---
+++ app/js/es5/common.js
@@ -0,0 +1,77 @@
+(function () {
+    'use strict';
+
+    $('document').ready(function () {
+        ///////////////////////////////////////////////////////////////////////////////////////////////////
+        $('.big').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            fade: true,
+            speed: 2000,
+            asNavFor: '.small',
+            autoplay: true,
+            autoplaySpeed: 4000,
+
+            responsive: [{
+                breakpoint: 1200,
+                settings: {}
+            }, {
+                breakpoint: 1062,
+                settings: {}
+            }, {
+                breakpoint: 768,
+                settings: {}
+            }]
+        });
+        var slideToSow = 6;
+        if ($(document).height() < 860) {
+            slideToSow = 5;
+        }
+        if ($(document).height() < 740) {
+            slideToSow = 4;
+        }
+        if ($(document).height() < 640) {
+            slideToSow = 3;
+        }
+
+        $('.small').slick({
+            slidesToShow: slideToSow,
+            slidesToScroll: true,
+            asNavFor: '.big',
+            dots: false,
+            infinite: true,
+            focusOnSelect: true,
+            prevArrow: '<span class="next"></span>',
+            nextArrow: '<span class="prev"></span>',
+            arrows: true,
+            vertical: true,
+            responsive: [{
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3
+                }
+            }]
+        });
+        ////////////////////////////////////////
+
+
+        var wrapperMenu = $('.wrapper-menu');
+
+        wrapperMenu.click(function () {
+            wrapperMenu.toggleClass('open');
+            $('.menu-box').toggleClass('menu-box-open');
+            $('header').toggleClass('header-after');
+        });
+        $('.menu-box ul li a ').click(function () {
+
+            setTimeout(function () {
+                wrapperMenu.toggleClass('open');
+                $('.menu-box').toggleClass('menu-box-open');
+                $('header').toggleClass('header-after');
+            }, 600);
+        });
+    });
+    console.log();
+})();
+
+//# sourceMappingURL=common.js.map
